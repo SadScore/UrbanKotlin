2f78c92ce9b6a78de8e38fd8233ee86038defa18
SadScore <sergei.aulov7@gmail.com>
2024-12-18 10:34:32 +0300

    Initial commit

--- src/StringClass/Task2.kt
+++ src/StringClass/Task2.kt
@@ -9,9 +9,16 @@ package StringClass
 fun main() {
     println(isNumberCorrect("+77777777777")) // true
     println(isNumberCorrect("+7777777758")) // false
+    println(isNumberCorrect("+7777777758yh")) // false
 
 }
 
 fun isNumberCorrect(phoneNumber: String): Boolean {
-    if (phoneNumber.startsWith('+') && phoneNumber[1] == '7' && phoneNumber.length == 12) return true else return false
+    if (isOnlyDigits(phoneNumber)) {
+        if (phoneNumber.startsWith('+') && phoneNumber[1] == '7' && phoneNumber.length == 12) return true else return false
+    }
+    return false
+}
+fun isOnlyDigits(str: String): Boolean {
+        return str.removeRange(0..1).all { it.isDigit() }
 }
